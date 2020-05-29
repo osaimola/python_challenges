0b20465b5f2af76d8a0894ddeb1663a9a8f22d4c
Osa <osaimola@gmail.com>
2020-05-28 18:33:44 -0600

    solve persistence challenge

--- challenges.py
+++ challenges.py
@@ -1,11 +1,49 @@
-"""Create a function that takes a 2D array as an argument and returns
+"""
+Create a function that takes a 2D array as an argument and returns
 the number of people whose view is blocked by a tall person. The concert
 stage is pointed towards the top of the 2D array and the tall person
 (represented by a 2) blocks the view of all the people (represented by a 1)
-behind them."""
+behind them.
+https://edabit.com/challenge/E9FwvGyad5CDbiH4C
+"""
 def block(lst):
 	rows = len(lst) - 1
 	blocked = 0
 	for i in range(rows):
 		blocked += (lst[i].count(2) * (rows - i))
 	return blocked
+
+
+"""
+The additive persistence of an integer, n, is the number of times you have to replace n with the sum of its digits until n becomes a single digit integer.
+The multiplicative persistence of an integer, n, is the number of times you have to replace n with the product of its digits until n becomes a single digit integer.
+
+Create two functions that take an integer as an argument and:
+1. return its additive persistence
+2. return its multiplicative persistence
+https://edabit.com/challenge/5ou6SKDtFRZugbpda
+"""
+def additive_persistence(n):
+	if n < 10:
+		return 0
+	
+	def sum_n(x):
+		result = 0
+		while x:
+			result, x = result + x % 10, x // 10
+		return result
+
+	return 1 + additive_persistence(sum_n(n))
+	
+
+def multiplicative_persistence(n):
+	if n < 10:
+		return 0
+	
+	def product_n(x):
+		result = 1
+		while x:
+			result, x = result * (x % 10), x // 10
+		return result
+
+	return 1 + multiplicative_persistence(product_n(n))
--- tests.py
+++ tests.py
@@ -20,5 +20,18 @@ class TestBlock(unittest.TestCase):
         [1, 1, 1, 1],
         ]), 4, "should be 4")
 
+    def test_additive_persistence(self):
+        self.assertEqual(additive_persistence(1679583), 3, "should be 3")
+
+        self.assertEqual(additive_persistence(123456), 2, "should be 2")
+
+
+    def test_multiplicative_persistence(self):
+        self.assertEqual(multiplicative_persistence(77), 4, "should be 4")
+
+        self.assertEqual(multiplicative_persistence(123456), 2, "should be 2")
+
+        self.assertEqual(multiplicative_persistence(4), 0, "should be 0")
+
 if __name__ == '__main__':
     unittest.main()
